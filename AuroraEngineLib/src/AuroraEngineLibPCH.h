26ff1e24d412f426898aa859a6e6c00fb60546bb
Duncan <duncanebauer@gmail.com>
2023-02-25 00:41:56 -0800

    commented pch. this is really unnecessary. and i still need to remove unused includes

--- AuroraEngineLib/src/AuroraEngineLibPCH.h
+++ AuroraEngineLib/src/AuroraEngineLibPCH.h
@@ -1,5 +1,40 @@
+// Aurora Engine Library, v0.0.1 ALPHA
+// Header file
+
+// For more information visit: https://github.com/DuncanBauer/ProjectAurora#table-of-contents
+
+// Developed by Duncan Bauer and every direct or indirect contributors to the GitHub.
+// LICENSING INFORMATION
+
+// It is recommended that you don't modify any source code! It will become difficult for you to update the library.
+// Note that 'Aurora::Util::' being a namespace, you can add functions into the namespace from your own source files, without
+// modifying Util.h or Util.cpp. Discussing your changes on the GitHub Issue Tracker may lead you
+// to a better solution or official support for them.
+
+// Index of this file:
+// CODE
+//   - [SECTION] PREPROCESSOR DIRECTIVES
+//   - [SECTION] INCLUDES
+//   - [SECTION] FORWARD DECLARATIONS
+//   - [SECTION] TYPENAMES
+//   - [SECTION] CONSTANTS
+//   - [SECTION] CLASSES
+//   - [SECTION] STRUCTS
+//   - [SECTION] FUNCTIONS
+//   - [SECTION] MACROS
+
+/************************************************************************************
+* [SECTION] PREPROCESSOR DIRECTIVES
+************************************************************************************/
 #pragma once
 
+/************************************************************************************
+* [SECTION] INCLUDES
+************************************************************************************/
+// Project Specific Headers
+#include <Core.h>
+
+// C++ Standard Library Headers
 #include <algorithm>
 #include <cstdint>
 #include <functional>
@@ -11,11 +46,32 @@
 #include <utility>
 #include <vector>
 
-#include <Core.h>
+// Third Party Library Headers
 
-//#ifdef PA_PLATFORM_WINDOWS
-//#endif
-//#ifdef AE_PLATFORM_MAC
-//#endif
-//#ifdef AE_PLATFORM_LINUX
-//#endif
+/************************************************************************************
+* [SECTION] FORWARD DECLARATIONS
+************************************************************************************/
+
+/************************************************************************************
+* [SECTION] TYPENAMES
+************************************************************************************/
+
+/************************************************************************************
+* [SECTION] CONSTANTS
+************************************************************************************/
+
+/************************************************************************************
+* [SECTION] CLASSES
+************************************************************************************/
+
+/************************************************************************************
+* [SECTION] STRUCTS
+************************************************************************************/
+
+/************************************************************************************
+* [SECTION] FUNCTIONS
+************************************************************************************/
+
+/************************************************************************************
+* [SECTION] MACROS
+************************************************************************************/
--- AuroraUtilLib/src/AuroraUtilLibPCH.h
+++ AuroraUtilLib/src/AuroraUtilLibPCH.h
@@ -1,5 +1,39 @@
+// Aurora Util Library, v0.0.1 ALPHA
+// Header file
+
+// For more information visit: https://github.com/DuncanBauer/ProjectAurora#table-of-contents
+
+// Developed by Duncan Bauer and every direct or indirect contributors to the GitHub.
+// LICENSING INFORMATION
+
+// It is recommended that you don't modify any source code! It will become difficult for you to update the library.
+// Note that 'Aurora::Util::' being a namespace, you can add functions into the namespace from your own source files, without
+// modifying Util.h or Util.cpp. Discussing your changes on the GitHub Issue Tracker may lead you
+// to a better solution or official support for them.
+
+// Index of this file:
+// CODE
+//   - [SECTION] PREPROCESSOR DIRECTIVES
+//   - [SECTION] INCLUDES
+//   - [SECTION] FORWARD DECLARATIONS
+//   - [SECTION] TYPENAMES
+//   - [SECTION] CONSTANTS
+//   - [SECTION] CLASSES
+//   - [SECTION] STRUCTS
+//   - [SECTION] FUNCTIONS
+//   - [SECTION] MACROS
+
+/************************************************************************************
+* [SECTION] PREPROCESSOR DIRECTIVES
+************************************************************************************/
 #pragma once
 
+/************************************************************************************
+* [SECTION] INCLUDES
+************************************************************************************/
+// Project Specific Headers
+
+// C++ Standard Library Headers
 #include <algorithm>
 #include <chrono>
 #include <cstdint>
@@ -11,3 +45,33 @@
 #include <unordered_set>
 #include <utility>
 #include <vector>
+
+// Third Party Library Headers
+
+/************************************************************************************
+* [SECTION] FORWARD DECLARATIONS
+************************************************************************************/
+
+/************************************************************************************
+* [SECTION] TYPENAMES
+************************************************************************************/
+
+/************************************************************************************
+* [SECTION] CONSTANTS
+************************************************************************************/
+
+/************************************************************************************
+* [SECTION] CLASSES
+************************************************************************************/
+
+/************************************************************************************
+* [SECTION] STRUCTS
+************************************************************************************/
+
+/************************************************************************************
+* [SECTION] FUNCTIONS
+************************************************************************************/
+
+/************************************************************************************
+* [SECTION] MACROS
+************************************************************************************/
